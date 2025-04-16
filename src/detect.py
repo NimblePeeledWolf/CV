c9a01f86f4a4665e117558cd5e5ef21fe480737b
NimblePeeledWolf <158261531+NimblePeeledWolf@users.noreply.github.com>
2025-04-16 05:34:14 +0000

    modified:   src/detect.py 	modified:   static/thumbnail_image0.jpg 	outputs/detected_sample_2.jpg

--- src/detect.py
+++ src/detect.py
@@ -3,12 +3,12 @@ import cv2
 from PIL import Image
 
 model = YOLO("yolov8n.pt")
-img_path = "/workspaces/CV/static/sample.jpg"
+img_path = "/workspaces/CV/static/thumbnail_image0.jpg"
 results = model(img_path)
 
 annotated_frame = results[0].plot()
 
-output_path = "/workspaces/CV/outputs/detected_sample.jpg"
+output_path = "/workspaces/CV/outputs/detected_sample_2.jpg"
 cv2.imwrite(output_path, annotated_frame)
 
 print(f"Detection Complete. Output saved to: {output_path}")
