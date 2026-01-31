740612070f04c6c3c60706b4ee51c76edc6f55ed
NimblePeeledWolf <158261531+NimblePeeledWolf@users.noreply.github.com>
2026-01-31 01:39:55 +0000

    updates

--- src/multiple_detect.py
+++ src/multiple_detect.py
@@ -7,10 +7,10 @@ import os
 
 model= YOLO("yolov8n.pt")
 
-results = model(["/workspaces/CV/practice nike"])
+results = model(["/workspaces/CV/nike"])
 
 for i, result in enumerate(results):
     annotated_frame = result.plot()
-    output_path = f"/workspaces/CV/outputs/detected_image{i+1}.jpeg"
+    output_path = f"/workspaces/CV/shoes/detected_image{i+1}.jpeg"
     cv2.imwrite(output_path, annotated_frame)
 
