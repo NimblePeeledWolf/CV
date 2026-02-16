6b531606f0c471aa8703730722170b3939c8a7ba
NimblePeeledWolf <158261531+NimblePeeledWolf@users.noreply.github.com>
2026-02-16 07:40:06 +0000

    working flask app

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from ultralytics import YOLO
 import os
 import uuid
@@ -6,15 +6,16 @@ import cv2
 
 
 app = Flask(__name__)
-
-model_path = os.environ.get('MODEL_PATH', 'best.pt')
-upload_dir = "static/uploads"
-output_dir = "static/outputs"
+MODEL_PATH = "/workspaces/CV/runs/detect/train4/weights/best.pt"
+model_path = os.environ.get("MODEL_PATH", MODEL_PATH)
+upload_dir = os.path.join(app.root_path, "static", "uploads")
+output_dir = os.path.join(app.root_path, "static", "outputs")
 
 os.makedirs(upload_dir, exist_ok=True)
 os.makedirs(output_dir, exist_ok=True)
 
 model = YOLO(model_path)
+
 
 @app.route("/", methods=["GET"])
 def home():
@@ -22,24 +23,23 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    if "image" not in request.files:
+    file = request.files.get("image")
+    if not file or file.filename == "":
         return "No file uploaded", 400
-    
-    file = request.files["image"]
-    if file.filename == "":
-        return "Empty filename", 400
-    
-    uid = uuid.uuid4.hex
-    in_path = os.path.join(upload_dir, f"{uid}jpg")
-    out_path = os.path.join(output_dir, f"{uid}jpg")
+
+    uid = uuid.uuid4().hex
+    in_filename = f"{uid}.jpg"
+    out_filename = f"{uid}_det.jpg"
+
+    in_path = os.path.join(upload_dir, in_filename)
+    out_path = os.path.join(output_dir, out_filename)
 
     file.save(in_path)
 
-    result = model(in_path, imgsz = 640)[0]
-    annotated = result.plot() 
+    result = model(in_path, imgsz=640, iou=0.20)[0]
+    annotated = result.plot()
     cv2.imwrite(out_path, annotated)
 
-    #extract detection summary
     detections = []
     names = result.names
     if result.boxes is not None and len(result.boxes) > 0:
@@ -48,7 +48,12 @@ def predict():
             conf = float(b.conf[0])
             detections.append({"label": names[cls_id], "conf": round(conf, 3)})
 
-    return render_template("result.html", input_image=in_path, output_image=out_path, detections=detections)
+    return render_template(
+        "result.html",
+        input_image=url_for("static", filename=f"uploads/{in_filename}"),
+        output_image=url_for("static", filename=f"outputs/{out_filename}"),
+        detections=detections
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
